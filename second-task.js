96028bcb2d41882e381a5dcf8a3bbe38d9195e2d
unknown <Mohammad-Abdelhadi>
2023-04-26 23:51:37 +0300

    done

---
+++ second-task.js
@@ -0,0 +1,24 @@
+let age = 27;
+if (age > 18) {
+  console.log("You are an adult");
+}
+console.log("____________________________________");
+let score = 45;
+if (score <= 50) {
+  console.log("You failed the test");
+}
+console.log("____________________________________");
+let fname = "mohammad";
+if (fname == "mohammad") {
+  console.log("hello mohammad");
+}
+console.log("____________________________________");
+let num = 4;
+if (num % 4 == 0) {
+  console.log("The number is even");
+}
+console.log("____________________________________");
+let char = "a";
+if (typeof char === "string") {
+  console.log("It's a letter");
+}
